097fead4ececd03fdbe4889243d2f6132e1e6a9a
Divesh <ds7699864@gmail.com>
2023-09-01 21:22:22 +0530

    updated a small mistake in js file

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
     });
 
     $(window).on('scroll load',function(){
-        $('menu').removeClass('fa-times');
+        $('#menu').removeClass('fa-times');
         $('header').removeClass('toggle');
 
         if($(window).scrollTop() > 0){
